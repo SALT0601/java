cda682aa4adbb35fe7af234dc59322f266fd65b1
SALT0601 <thfxm0601@gmail.com>
2022-01-05 13:21:12 +0900

    java_test

---
+++ src/java_test/Hello.java
@@ -0,0 +1,16 @@
+package java_test;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\    /\\\r\n"
+				+ " )  ( ')\r\n"
+				+ "(  /  )\r\n"
+				+ " \\(__)|");
+		
+		
+
+	}
+
+}
---
+++ src/mc/ys/s1309/test/Test.java
@@ -0,0 +1,11 @@
+package mc.ys.s1309.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome, Hello World!");
+
+	}
+
+}
